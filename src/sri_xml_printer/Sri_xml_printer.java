1b2be73b930472f73d08b0fd7ad4de8c0c9807e5
adarpis <>
2019-04-25 17:02:14 -0500

    Changed author and added WatchService

--- src/sri_xml_printer/Sri_xml_printer.java
+++ src/sri_xml_printer/Sri_xml_printer.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2019 aarpi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package sri_xml_printer;
 
@@ -163,7 +174,7 @@ public class Sri_xml_printer {
     }
 
     //@SuppressWarnings("unchecked")
-    private void doPrint() {
+    public void doPrint() {
 	PrinterJob pj = PrinterJob.getPrinterJob();
 	pj.setPrintable(new BillPrintable(), getPageFormat(pj));
 	try {
@@ -179,7 +190,7 @@ public class Sri_xml_printer {
      */
     public static void main(String[] args) {
 	Sri_xml_printer test = new Sri_xml_printer();
-	test.doPrint();
+	//test.doPrint();
     }
 
 }
---
+++ src/watchDir/WatchServiceDir.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2019 aarpi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package watchDir;
+
+/**
+ *
+ * @author aarpi
+ */
+import static java.nio.file.StandardWatchEventKinds.*;
+ 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+import sri_xml_printer.Sri_xml_printer;
+ 
+public class WatchServiceDir {
+ 
+    private final WatchService watcher;
+    private final Map<WatchKey, Path> keys;
+ 
+    /**
+     * Creates a WatchService and registers the given directory
+     */
+    WatchServiceDir(Path dir) throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<WatchKey, Path>();
+ 
+        walkAndRegisterDirectories(dir);
+    }
+ 
+    /**
+     * Register the given directory with the WatchService; This function will be called by FileVisitor
+     */
+    private void registerDirectory(Path dir) throws IOException
+    {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        keys.put(key, dir);
+    }
+ 
+    /**
+     * Register the given directory, and all its sub-directories, with the WatchService.
+     */
+    private void walkAndRegisterDirectories(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                registerDirectory(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+ 
+    /**
+     * Process all events for keys queued to the watcher
+     */
+    void processEvents() {
+	Sri_xml_printer printer = new Sri_xml_printer();
+        for (;;) {
+ 
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+ 
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+ 
+            for (WatchEvent<?> event : key.pollEvents()) {
+                @SuppressWarnings("rawtypes")
+                WatchEvent.Kind kind = event.kind();
+ 
+                // Context for directory entry event is the file name of entry
+                @SuppressWarnings("unchecked")
+                Path name = ((WatchEvent<Path>)event).context();
+                Path child = dir.resolve(name);
+ 
+                // print out event
+                System.out.format("%s: %s\n", event.kind().name(), child);
+		
+		if (kind == ENTRY_MODIFY) {
+		    printer.doPrint();
+		}
+ 
+                // if directory is created, and watching recursively, then register it and its sub-directories
+                if (kind == ENTRY_CREATE) {
+                    try {
+                        if (Files.isDirectory(child)) {
+                            walkAndRegisterDirectories(child);
+                        }
+                    } catch (IOException x) {
+                        // do something useful
+                    }
+                }
+            }
+ 
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+ 
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        Path dir = Paths.get("C:\\Users\\dortega\\Desktop\\dev180920\\ePOS_Print_dev\\sri_printer\\dirs\\gen");
+        new WatchServiceDir(dir).processEvents();
+    }
+}
