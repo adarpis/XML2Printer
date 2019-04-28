a039e7bb1570b1f78e5bf449126c02a7071d8e32
adarpis <adrian.d.arpi.s1@gmail.com>
2019-04-28 00:20:12 -0500

    With printer for infoTributaria With parameters on project properties

--- src/sri_xml_printer/Sri_xml_printer.java
+++ src/sri_xml_printer/Sri_xml_printer.java
@@ -26,6 +26,13 @@ import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import java.awt.FontMetrics;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import xml.XMLReader;
+
 /**
  *
  * @author aarpi
@@ -34,138 +41,280 @@ public class Sri_xml_printer {
 
     public class BillPrintable implements Printable {
 
-	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
-		throws PrinterException {
+        private Document xmldoc;
 
-	    int result = NO_SUCH_PAGE;
-	    if (pageIndex == 0) {
+        public void setXMLDoc(String file) {
+            XMLReader xreader = new XMLReader();
+            this.xmldoc = xreader.docXMLBill(file);
+        }
 
-		Graphics2D g2d = (Graphics2D) graphics;
+        public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
+                throws PrinterException {
 
-		//double width = pageFormat.getImageableWidth();
+            int result = NO_SUCH_PAGE;
 
-		g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
+            if (pageIndex == 0 && xmldoc != null) {
 
-		try {
-		    /*Draw Header*/
-		    int y = 20;
-		    final int yShift = 10;
-		    final int headerRectHeight = 15;
+                Element root = xmldoc.getDocumentElement();
+                System.out.println("Root element :" + root.getNodeName() + " " + root.getAttribute("id") + " " + root.getAttribute("version"));
+                if (root.getAttribute("id").equals("comprobante") && root.getAttribute("version").equals("1.0.0")) {
+//                    System.out.println("----------------------------");
+//
+//                    NodeList nList = xmldoc.getElementsByTagName("infoTributaria");
+//
+//                    for (int temp = 0; temp < nList.getLength(); temp++) {
+//
+//                        Node nNode = nList.item(temp);
+//
+//                        System.out.println("\nCurrent Element :" + nNode.getNodeName());
+//
+//                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//
+//                            Element eElement = (Element) nNode;
+//
+//                            System.out.println("nombreComercial : " + eElement.getElementsByTagName("nombreComercial").item(0).getTextContent());
+//                            System.out.println("razonSocial : " + eElement.getElementsByTagName("razonSocial").item(0).getTextContent());
+//                            System.out.println("ruc : " + eElement.getElementsByTagName("ruc").item(0).getTextContent());
+//                            System.out.println("claveAcceso : " + eElement.getElementsByTagName("claveAcceso").item(0).getTextContent());
+//                            System.out.println("factura : " + eElement.getElementsByTagName("estab").item(0).getTextContent()
+//                                    + "-" + eElement.getElementsByTagName("ptoEmi").item(0).getTextContent()
+//                                    + "-" + eElement.getElementsByTagName("secuencial").item(0).getTextContent());
+//                            System.out.println("dirMatriz : " + eElement.getElementsByTagName("dirMatriz").item(0).getTextContent());
+//
+//                        }
+//                    }
+//
+//                    nList = xmldoc.getElementsByTagName("infoFactura");
+//
+//                    for (int temp = 0; temp < nList.getLength(); temp++) {
+//
+//                        Node nNode = nList.item(temp);
+//
+//                        System.out.println("\nCurrent Element :" + nNode.getNodeName());
+//
+//                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//
+//                            Element eElement = (Element) nNode;
+//
+//                            System.out.println("fechaEmision : " + eElement.getElementsByTagName("fechaEmision").item(0).getTextContent());
+//                            System.out.println("dirEstablecimiento : " + eElement.getElementsByTagName("dirEstablecimiento").item(0).getTextContent());
+//                            System.out.println("razonSocialComprador : " + eElement.getElementsByTagName("razonSocialComprador").item(0).getTextContent());
+//                            System.out.println("identificacionComprador : " + eElement.getElementsByTagName("identificacionComprador").item(0).getTextContent());
+//                            if (!eElement.getElementsByTagName("tipoIdentificacionComprador").item(0).getTextContent().equals("07")) {
+//                                System.out.println("direccionComprador : " + eElement.getElementsByTagName("direccionComprador").item(0).getTextContent());
+//                            }
+//                            System.out.println("totalSinImpuestos : " + eElement.getElementsByTagName("totalSinImpuestos").item(0).getTextContent());
+//                            System.out.println("importeTotal : " + eElement.getElementsByTagName("importeTotal").item(0).getTextContent());
+//
+//                        }
+//                    }
+//
+//                    nList = xmldoc.getElementsByTagName("detalle");
+//
+//                    for (int temp = 0; temp < nList.getLength(); temp++) {
+//
+//                        Node nNode = nList.item(temp);
+//
+//                        System.out.println("\nCurrent Element :" + nNode.getNodeName());
+//
+//                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//
+//                            Element eElement = (Element) nNode;
+//
+//                            System.out.println("item : " + eElement.getElementsByTagName("cantidad").item(0).getTextContent()
+//                                    + "\t" + eElement.getElementsByTagName("descripcion").item(0).getTextContent()
+//                                    + "\t" + eElement.getElementsByTagName("precioUnitario").item(0).getTextContent()
+//                                    + "\t" + eElement.getElementsByTagName("precioTotalSinImpuesto").item(0).getTextContent());
+//
+//                        }
+//                    }
+//
+//                    nList = xmldoc.getElementsByTagName("totalImpuesto");
+//
+//                    for (int temp = 0; temp < nList.getLength(); temp++) {
+//
+//                        Node nNode = nList.item(temp);
+//
+//                        System.out.println("\nCurrent Element :" + nNode.getNodeName());
+//
+//                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//
+//                            Element eElement = (Element) nNode;
+//
+//                            System.out.println("baseImponible : " + eElement.getElementsByTagName("baseImponible").item(0).getTextContent());
+//                            System.out.println("tarifa : " + eElement.getElementsByTagName("tarifa").item(0).getTextContent());
+//                            System.out.println("valor : " + eElement.getElementsByTagName("valor").item(0).getTextContent());
+//
+//                        }
+//                    }
+//
+//                }
 
-		    ///////////////// Product names Get ///////////
-		    String pn1a = "test 1";
-		    String pn2a = "test 2";
-		    String pn3a = "test 3";
-		    String pn4a = "test 4";
-		    ///////////////// Product names Get ///////////
+                    Graphics2D g2d = (Graphics2D) graphics;
 
-		    ///////////////// Product price Get ///////////
-		    int pp1a = 10;
-		    int pp2a = 20;
-		    int pp3a = 30;
-		    int pp4a = 40;
-		    int sum = pp1a + pp2a + pp3a + pp4a;
-		    ///////////////// Product price Get ///////////
+                    //double width = pageFormat.getImageableWidth();
+                    g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
 
-		    g2d.setFont(new Font("Monospaced", Font.PLAIN, 9));
-		    g2d.drawString("-------------------------------------", 12, y);
-		    y += yShift;
-		    g2d.drawString("      Restaurant Bill Receipt        ", 12, y);
-		    y += yShift;
-		    g2d.drawString("-------------------------------------", 12, y);
-		    y += headerRectHeight;
+                    try {
+                        /*Draw Header*/
+                        int y = 20;
+                        final int yShift = 10;
+                        final int headerRectHeight = 15;
 
-		    g2d.drawString("-------------------------------------", 10, y);
-		    y += yShift;
-		    g2d.drawString(" Food Name                 T.Price   ", 10, y);
-		    y += yShift;
-		    g2d.drawString("-------------------------------------", 10, y);
-		    y += headerRectHeight;
-		    g2d.drawString(" " + pn1a + "                  " + pp1a + "  ", 10, y);
-		    y += yShift;
-		    g2d.drawString(" " + pn2a + "                  " + pp2a + "  ", 10, y);
-		    y += yShift;
-		    g2d.drawString(" " + pn3a + "                  " + pp3a + "  ", 10, y);
-		    y += yShift;
-		    g2d.drawString(" " + pn4a + "                  " + pp4a + "  ", 10, y);
-		    y += yShift;
-		    g2d.drawString("-------------------------------------", 10, y);
-		    y += yShift;
-		    g2d.drawString(" Total amount: " + sum + "               ", 10, y);
-		    y += yShift;
-		    g2d.drawString("-------------------------------------", 10, y);
-		    y += yShift;
-		    g2d.drawString("          Free Home Delivery         ", 10, y);
-		    y += yShift;
-		    g2d.drawString("             03111111111             ", 10, y);
-		    y += yShift;
-		    g2d.drawString("*************************************", 10, y);
-		    y += yShift;
-		    g2d.drawString("    THANKS TO VISIT OUR RESTUARANT   ", 10, y);
-		    y += yShift;
-		    g2d.drawString("*************************************", 10, y);
-		    //y += yShift;
+                        ///////////////// Product names Get ///////////
+                        String pn1a = "test 1";
+                        String pn2a = "test 2";
+                        String pn3a = "test 3";
+                        String pn4a = "test 4";
+                        ///////////////// Product names Get ///////////
 
-		} catch (Exception r) {
-		    r.printStackTrace();
-		}
+                        ///////////////// Product price Get ///////////
+                        int pp1a = 10;
+                        int pp2a = 20;
+                        int pp3a = 30;
+                        int pp4a = 40;
+                        int sum = pp1a + pp2a + pp3a + pp4a;
+                        ///////////////// Product price Get ///////////
 
-		result = PAGE_EXISTS;
-	    }
-	    return result;
-	}
+                        g2d.setFont(new Font("Monospaced", Font.PLAIN, 9));
+
+                        NodeList nList = xmldoc.getElementsByTagName("infoTributaria");
+
+                        for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                            Node nNode = nList.item(temp);
+
+                            System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                                Element eElement = (Element) nNode;
+
+                                g2d.drawString("-------------------------------------", 12, y);
+                                y += yShift;
+                                g2d.drawString("      " + eElement.getElementsByTagName("nombreComercial").item(0).getTextContent() + "        ", 12, y);
+                                y += yShift;
+                                g2d.drawString("      " + eElement.getElementsByTagName("razonSocial").item(0).getTextContent(), 12, y);
+                                y += yShift;
+                                g2d.drawString("RUC: " + eElement.getElementsByTagName("ruc").item(0).getTextContent(), 12, y);
+                                y += yShift;
+                                g2d.drawString(eElement.getElementsByTagName("claveAcceso").item(0).getTextContent(), 5, y);
+                                y += yShift;
+                                g2d.drawString("FACTURA # " + eElement.getElementsByTagName("estab").item(0).getTextContent()
+                                        + "-" + eElement.getElementsByTagName("ptoEmi").item(0).getTextContent()
+                                        + "-" + eElement.getElementsByTagName("secuencial").item(0).getTextContent(), 12, y);
+                                y += yShift;
+                                g2d.drawString("MATRIZ: " + eElement.getElementsByTagName("dirMatriz").item(0).getTextContent(), 12, y);
+                                y += yShift;
+                                g2d.drawString("-------------------------------------", 12, y);
+                                y += headerRectHeight;
+
+                                System.out.println("nombreComercial : " + eElement.getElementsByTagName("nombreComercial").item(0).getTextContent());
+                                System.out.println("razonSocial : " + eElement.getElementsByTagName("razonSocial").item(0).getTextContent());
+                                System.out.println("ruc : " + eElement.getElementsByTagName("ruc").item(0).getTextContent());
+                                System.out.println("claveAcceso : " + eElement.getElementsByTagName("claveAcceso").item(0).getTextContent());
+                                System.out.println("factura : " + eElement.getElementsByTagName("estab").item(0).getTextContent()
+                                        + "-" + eElement.getElementsByTagName("ptoEmi").item(0).getTextContent()
+                                        + "-" + eElement.getElementsByTagName("secuencial").item(0).getTextContent());
+                                System.out.println("dirMatriz : " + eElement.getElementsByTagName("dirMatriz").item(0).getTextContent());
+
+                            }
+                        }
+
+                        g2d.drawString("-------------------------------------", 10, y);
+                        y += yShift;
+                        g2d.drawString(" Food Name                 T.Price   ", 10, y);
+                        y += yShift;
+                        g2d.drawString("-------------------------------------", 10, y);
+                        y += headerRectHeight;
+                        g2d.drawString(" " + pn1a + "                  " + pp1a + "  ", 10, y);
+                        y += yShift;
+                        g2d.drawString(" " + pn2a + "                  " + pp2a + "  ", 10, y);
+                        y += yShift;
+                        g2d.drawString(" " + pn3a + "                  " + pp3a + "  ", 10, y);
+                        y += yShift;
+                        g2d.drawString(" " + pn4a + "                  " + pp4a + "  ", 10, y);
+                        y += yShift;
+                        g2d.drawString("-------------------------------------", 10, y);
+                        y += yShift;
+                        g2d.drawString(" Total amount: " + sum + "               ", 10, y);
+                        y += yShift;
+                        g2d.drawString("-------------------------------------", 10, y);
+                        y += yShift;
+                        g2d.drawString("          Free Home Delivery         ", 10, y);
+                        y += yShift;
+                        g2d.drawString("             03111111111             ", 10, y);
+                        y += yShift;
+                        g2d.drawString("*************************************", 10, y);
+                        y += yShift;
+                        g2d.drawString("    THANKS TO VISIT OUR RESTUARANT   ", 10, y);
+                        y += yShift;
+                        g2d.drawString("*************************************", 10, y);
+                        //y += yShift;
+
+                    } catch (Exception r) {
+                        r.printStackTrace();
+                    }
+
+                }
+
+                result = PAGE_EXISTS;
+            }
+            return result;
+        }
 
     }
 
     protected static double convert_CM_To_PPI(double cm) {
-	return toPPI(cm * 0.393600787);
+        return toPPI(cm * 0.393600787);
     }
 
     protected static double toPPI(double inch) {
-	return inch * 72d;
+        return inch * 72d;
     }
 
     public PageFormat getPageFormat(PrinterJob pj) {
 
-	PageFormat pf = pj.defaultPage();
-	Paper paper = pf.getPaper();
+        PageFormat pf = pj.defaultPage();
+        Paper paper = pf.getPaper();
 
-	double middleHeight = 8.0;
-	double headerHeight = 2.0;
-	double footerHeight = 2.0;
-	double width = convert_CM_To_PPI(8);      //printer know only point per inch.default value is 72ppi
-	double height = convert_CM_To_PPI(headerHeight + middleHeight + footerHeight);
-	paper.setSize(width, height);
-	paper.setImageableArea(
-		0,
-		10,
-		width,
-		height - convert_CM_To_PPI(1)
-	);   //define boarder size    after that print area width is about 180 points
+        double middleHeight = 8.0;
+        double headerHeight = 2.0;
+        double footerHeight = 2.0;
+        double width = convert_CM_To_PPI(8);      //printer know only point per inch.default value is 72ppi
+        double height = convert_CM_To_PPI(headerHeight + middleHeight + footerHeight);
+        paper.setSize(width, height);
+        paper.setImageableArea(
+                0,
+                10,
+                width,
+                height - convert_CM_To_PPI(1)
+        );   //define boarder size    after that print area width is about 180 points
 
-	pf.setOrientation(PageFormat.PORTRAIT);           //select orientation portrait or landscape but for this time portrait
-	pf.setPaper(paper);
+        pf.setOrientation(PageFormat.PORTRAIT);           //select orientation portrait or landscape but for this time portrait
+        pf.setPaper(paper);
 
-	return pf;
+        return pf;
     }
 
-    //@SuppressWarnings("unchecked")
-    public void doPrint() {
-	PrinterJob pj = PrinterJob.getPrinterJob();
-	pj.setPrintable(new BillPrintable(), getPageFormat(pj));
-	try {
-	    pj.print();
-	} catch (PrinterException ex) {
-	    ex.printStackTrace();
-	}
+    public void doPrint(String file) {
+        PrinterJob pj = PrinterJob.getPrinterJob();
+        BillPrintable bPrint = new BillPrintable();
+        bPrint.setXMLDoc(file);
+        pj.setPrintable(bPrint, getPageFormat(pj));
+        try {
+            pj.print();
+        } catch (PrinterException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	Sri_xml_printer test = new Sri_xml_printer();
-	test.doPrint();
+        Sri_xml_printer PrintTest = new Sri_xml_printer();
+        PrintTest.doPrint(args[0]);
     }
 
 }
--- src/watchDir/WatchServiceDir.java
+++ src/watchDir/WatchServiceDir.java
@@ -21,7 +21,7 @@ package watchDir;
  * @author aarpi
  */
 import static java.nio.file.StandardWatchEventKinds.*;
- 
+
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
@@ -38,33 +38,34 @@ import java.util.Map;
 
 import sri_xml_printer.Sri_xml_printer;
 import xml.XMLReader;
- 
+
 public class WatchServiceDir {
- 
+
     private final WatchService watcher;
     private final Map<WatchKey, Path> keys;
- 
+
     /**
      * Creates a WatchService and registers the given directory
      */
     WatchServiceDir(Path dir) throws IOException {
         this.watcher = FileSystems.getDefault().newWatchService();
         this.keys = new HashMap<WatchKey, Path>();
- 
+
         walkAndRegisterDirectories(dir);
     }
- 
+
     /**
-     * Register the given directory with the WatchService; This function will be called by FileVisitor
+     * Register the given directory with the WatchService; This function will be
+     * called by FileVisitor
      */
-    private void registerDirectory(Path dir) throws IOException
-    {
+    private void registerDirectory(Path dir) throws IOException {
         WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
         keys.put(key, dir);
     }
- 
+
     /**
-     * Register the given directory, and all its sub-directories, with the WatchService.
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
      */
     private void walkAndRegisterDirectories(final Path start) throws IOException {
         // register directory and sub-directories
@@ -76,15 +77,15 @@ public class WatchServiceDir {
             }
         });
     }
- 
+
     /**
      * Process all events for keys queued to the watcher
      */
     void processEvents() {
-	Sri_xml_printer printer = new Sri_xml_printer();
-        XMLReader reader = new XMLReader();
+        Sri_xml_printer printer = new Sri_xml_printer();
+        //XMLReader reader = new XMLReader();
         for (;;) {
- 
+
             // wait for key to be signalled
             WatchKey key;
             try {
@@ -92,30 +93,30 @@ public class WatchServiceDir {
             } catch (InterruptedException x) {
                 return;
             }
- 
+
             Path dir = keys.get(key);
             if (dir == null) {
                 System.err.println("WatchKey not recognized!!");
                 continue;
             }
- 
+
             for (WatchEvent<?> event : key.pollEvents()) {
                 @SuppressWarnings("rawtypes")
                 WatchEvent.Kind kind = event.kind();
- 
+
                 // Context for directory entry event is the file name of entry
                 @SuppressWarnings("unchecked")
-                Path name = ((WatchEvent<Path>)event).context();
+                Path name = ((WatchEvent<Path>) event).context();
                 Path child = dir.resolve(name);
- 
+
                 // print out event
                 System.out.format("%s: %s\n", event.kind().name(), child);
-		
-		if (kind == ENTRY_MODIFY) {
-                    reader.ReadXMLBill(child.toString());
-		    printer.doPrint();
-		}
- 
+
+                if (kind == ENTRY_MODIFY) {
+                    //reader.ReadXMLBill(child.toString());
+                    printer.doPrint(child.toString());
+                }
+
                 // if directory is created, and watching recursively, then register it and its sub-directories
                 if (kind == ENTRY_CREATE) {
                     try {
@@ -127,12 +128,12 @@ public class WatchServiceDir {
                     }
                 }
             }
- 
+
             // reset key and remove from set if directory no longer accessible
             boolean valid = key.reset();
             if (!valid) {
                 keys.remove(key);
- 
+
                 // all directories are inaccessible
                 if (keys.isEmpty()) {
                     break;
@@ -140,9 +141,11 @@ public class WatchServiceDir {
             }
         }
     }
- 
+
     public static void main(String[] args) throws IOException {
-        Path dir = Paths.get("C:\\Users\\adria\\Documents\\aarpi\\temp\\dirs\\gen");
-        new WatchServiceDir(dir).processEvents();
+        if (args.length > 0) {
+            Path dir = Paths.get(args[0]);
+            new WatchServiceDir(dir).processEvents();
+        }
     }
 }
